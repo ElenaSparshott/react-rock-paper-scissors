a8b8dcb51eef306a29d4357acb2a970ad3f6e98c
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-04-19 21:57:13 +0100

    first version

--- src/App.js
+++ src/App.js
@@ -1,28 +1,63 @@
 import './App.css';
 import { useState } from 'react'
-function App() {
 
-  const [userChoice, setuserChoice] = useState(null)
+function App() {
+  const [computerChoice, setComputerChoice] = useState(null)
+  const [userChoice, setUserChoice] = useState(null)
+  const [result, setResult] = useState(null)
+
   const choices = ['rock', 'paper', 'scissors']
 
   const handleClick = (value) => {
-      setuserChoice(value)
+      setUserChoice(value)
+      generateComputerChoice()
+      checkResult()
   }
 
   const generateComputerChoice = () => {
-    
+    const randomChoice = choices[Math.floor(Math.random() * choices.length)]
+    setComputerChoice(randomChoice)
   }
 
-
+  const checkResult = () => {
+    if(userChoice === computerChoice){
+      setResult("You Draw")
+    }
+    switch(userChoice){
+      case "rock":
+        switch(computerChoice){
+          case 'paper': setResult("You Lose")
+          break
+          case 'scissors': setResult("You Win")
+          break
+        }
+      case "paper":
+        switch(computerChoice){
+          case 'scissors': setResult("You Lose")
+          break
+          case 'rock': setResult("You Win")
+          break
+        } 
+      case "scissors":
+        switch(computerChoice){
+          case "rock": setResult('You Win')
+          break
+          case "paper": setResult('You Lose')
+          break
+        }
+    }
+  }
 
   return (
     <div>
-      <h1>user choice is: </h1>
-      <h1>computer choice is: {userChoice}</h1>
-      {/* <button onClick={() => handleclick('rock')}>Rock</button>
-      <button onClick={() => handleclick('paper')}>Paper</button>
-      <button onClick={() => handleclick('scissors')}>Scissors</button> */}
+      <h1>user choice is: {userChoice}</h1>
       {choices.map((choice, index) => <button key={index}onClick={() => handleClick(choice)}>{choice}</button>)}
+
+      <h1>computer choice is: {computerChoice}</h1>
+
+      <h1>{result}</h1>
+      
+      
     </div>
   );
 }
